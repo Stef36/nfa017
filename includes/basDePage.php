d5d075cf864060c67aad38a5d5d7029418856cc1
domduf <minique.duf@gmail.com>
2017-03-18 17:33:50 +0100

    modif bas de page

--- includes/basDePage.php
+++ includes/basDePage.php
@@ -1,8 +1,8 @@
 <footer>
 <!--Une ligne pour separer le bas de page
 <hr/>-->
-<p>site du groupe Soul Latitude.</p>
-
+<p>site exercice du CNAM NFA017-083-021.</p>
+<p>Dominique D. et Stéphane L.</p>
 <a href="./planDuSite.php">Plan du site et documents utiles.</a><br/>
 <a href="./mentionsLegales.php">Mentions légales.</a><br/>
 <a href="./notesVersion.php">Notes de versions et derniers changements</a><br/>
