1865bf2ad73d4cc89e1b08ccba5083faa2736fc4
domduf <minique.duf@gmail.com>
2017-04-11 20:51:24 +0200

    continue adaptation administration_equipes_modif.php

--- administration_equipes_modif.php
+++ administration_equipes_modif.php
@@ -1,3 +1,5 @@
+<?php Session_start()?>
+
 <!DOCTYPE html>
 <html lang="fr-fr">
 
@@ -13,7 +15,7 @@
 	<link rel="icon" href="mesRepos.ico" />
 
 	<title>Mes Repos | modif équipes </title>
-	<link href="./css/soul.css" rel="stylesheet" type="text/css" />
+	<link href="./css/style.css" rel="stylesheet" type="text/css" />
 </head>
 <!-- ======================================================= -->
 
@@ -25,7 +27,7 @@
 
 <!-- ===================== TITRE ===================== -->
 
-<?php require ("./includes/header.inc.php"); titre_header('Administration </br>modif. d\'une équipe')?>
+<?php require ("./includes/header.inc.php"); titre_header('Administration </br>modification d\'une équipe')?>
 
 
 <!-- ===================== VISUEL ===================== -->
@@ -40,57 +42,61 @@
   	 
   	if ( isset($_SESSION['login'])) {
   	
- 	$choixInstru=$_POST ['choixInstru'];
- 	$_SESSION['choixInstru']=$choixInstru;
+ 	$choixEquipe=$_POST ['choixEquipe'];
+ 	$_SESSION['choixEquipe']=$choixEquipe;
 
   	$login=$_SESSION['login'];
   	$mdp=$_SESSION['mdp'];
   	
-  	$sql_instru = "	SELECT 		*
-						FROM 		instrument
-						WHERE 		inst_id= '$choixInstru' " ;
+  	$sql_equipe = "	SELECT 		*
+						FROM 		equipe
+						WHERE 		equipe_id= '$choixEquipe' " ;
 			
-  	$instruments= $pdo->query($sql_instru); ?>
+  	$equipes= $pdo->query($sql_equipe); ?>
  	
 
   	
 	<?php
 	/* test de login et mdp */
-	if ($instrument = $instruments->fetch()) {
+	if ($equipe = $equipes->fetch()) {
 	
-  			 /*echo ('membre: '.$instrument['mem_prenom']); test*/?>
+  			 /*echo ('membre: '.$equipe['mem_prenom']); test*/?>
   
     <span id="administration"> 
-		<?php echo (' id choix instrument: '. $choixInstru); ?>
-		<H2><?php echo ('Modification de l\'instrument '.$instrument['inst_nom'].' type '.$instrument['inst_type'].' (id '.$instrument['inst_id'].')');?><br> 
+
+		<?php echo (' id choix equipe: '. $choixEquipe); ?>
+
+		<H2><?php echo ('Modification de l\'equipe: '.$equipe['equipe_nom'].
+		'<br/> login '.$equipe['equipe_login'].' (id '.$equipe['equipe_id'].')');?><br> 
 		
-			<?php	$_SESSION['vientDeAdminContenu']=1;
-					$_SESSION ['flag_requete_update_instrument']=0 ?> </H2>
+			<?php	$_SESSION['vientDeBackoffice']=1;
+					$_SESSION ['flag_requete_update_equipe']=0 ?> </H2>
 
 <!-- ==================================================================  -->
-<p><a href="administration_liste_contenu.php"><img src="./images/boutonRetour.png"></a><p>
+<!-- bouton de retour au choix d'administration -->
+<p><a href="backoffice.php"><img src="./images/boutons/retour.png"></a><p>
 <!-- ==================================================================  -->
 
   		 		
   		<p>Attention, toute modification influe sur le contenu de la base de donnée...</br>
   		Soyez sûr de ce que vous faites.</p>
   		
-  	<form name="modifInstrument"  method="POST" action="./administration_modif_instrument_validation.php" >
+  	<form name="modifEquipe"  method="POST" action="./administration_modif_equipe_validation.php" >
   	
   		<table id="tableau_messages">
 			<tr>
 				
-				<th>inst_id</th>
-				<td><?php echo ($instrument['inst_id']); ?></td>	
+				<th>equipe_id</th>
+				<td><?php echo ($equipe['equipe_id']); ?></td>	
 			</tr>
 				
 			<tr>
-				<th>inst_nom</th>
+				<th>equipe_nom</th>
 				<td>
-				<select name="inst_nom" id ="inst_nom">
+				<select name="equipe_nom" id ="equipe_nom">
 		 			<?php 
 
-					/* tableau des choix de nom d'instrument */
+					/* tableau des choix de nom d'equipe */
 					
 					$choix = array('chant','flûte','clarinette','saxophone','trompette','trombone',
 									'tuba','guitare','basse','batterie','percussions','piano / orgue',
@@ -100,7 +106,7 @@
 					foreach ($choix as $choi) {	 ?>
 							<option value="<?php echo ($choi); ?>" 
 									<?php 
-										if ($choi == $instrument['inst_nom']) { ?>
+										if ($choi == $equipe['equipe_nom']) { ?>
 										 selected="selected" 
 										<?php } ?> 
 										><?php
@@ -115,30 +121,30 @@
 
 
 			<tr>
-					<th>inst_type</th>
+					<th>equipe_type</th>
 					<td>
 					<input type="text" 
-		   				 name="inst_type" value="<?php echo ($instrument['inst_type']); ?>" 
-		    id="inst_type"/>
+		   				 name="equipe_type" value="<?php echo ($equipe['equipe_type']); ?>" 
+		    id="equipe_type"/>
 					</td>
 			</tr>	
 				
 			<tr>
-					<th>inst_marque</th>
+					<th>equipe_marque</th>
 					<td>
 					<input type="text" 
-		   				 name="inst_marque" value="<?php echo ($instrument['inst_marque']); ?>" 
-		    id="inst_marque"/>
+		   				 name="equipe_marque" value="<?php echo ($equipe['equipe_marque']); ?>" 
+		    id="equipe_marque"/>
 					</td>
 			</tr>
 
 	
 			<tr>
-					<th>inst_no_serie</th>
+					<th>equipe_no_serie</th>
 					<td>
 					<input type="text" 
-		   				 name="inst_no_serie" value="<?php echo ($instrument['inst_no_serie']); ?>" 
-		    id="inst_no_serie"/>
+		   				 name="equipe_no_serie" value="<?php echo ($equipe['equipe_no_serie']); ?>" 
+		    id="equipe_no_serie"/>
 					</td>
 			</tr>	
 					
@@ -149,7 +155,8 @@
   	</form>
 
 <!-- ==================================================================  -->
-<p><a href="administration_liste_contenu.php"><img src="./images/boutonRetour.png"></a><p>
+<!-- bouton de retour au choix d'administration -->
+<p><a href="backoffice.php"><img src="./images/boutons/retour.png"></a><p>
 <!-- ==================================================================  -->
 
   		
@@ -164,7 +171,9 @@
 	else {	?>
 	<span>
 		<H2>Désolé <?php echo ($_SESSION['login']);?>, mot de passe et/ou login incorrect...
-		<a href="connection_back_office.php"><img src="./images/boutonRetour.png"></a></H2>
+
+<!-- bouton de retour au choix d'administration -->
+<p><a href="backoffice.php"><img src="./images/boutons/retour.png"></a><p>
 		
 	</span>
 		
