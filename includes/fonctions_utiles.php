05d10e08cb9d2df61f9c4d3324cd4e9b89a74381
domduf <minique.duf@gmail.com>
2017-04-17 11:29:56 +0200

    debut essais afficher logos dans page includes/visualiser-equipe_admin.inc.php

---
+++ includes/fonctions_utiles.php
@@ -0,0 +1,40 @@
+<?php
+
+/* fonction d'affichage suivant le type MIME  */
+
+	function afficher_suivant_mime($fichier, $credit, $id, $class, $alt) {
+
+		$typeMime = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $fichier) ;
+
+	if ( $typeMime == 'image/jpeg' OR $typeMime =='image/png'OR $typeMime =='image/gif') 
+
+		{?>
+		<p>
+		<img id = "<?php echo $id; ?>" class= "<?php echo $class;?>" src="<?php echo $fichier ;?>" alt="<?php echo $alt; ?> www.mesrepos.domduf.com"/>
+		</p>
+
+		<p><?php echo $typeMime ; ?></p>
+			<?php 
+			if ($credit){ ?>
+			<p>Photo &copy <?php echo $credit ;?></p>
+			<?php }?>
+		<?php 
+		}
+	
+	elseif ( $typeMime == 'video/webm' OR $typeMime == 'video/mp4' OR $typeMime == 'video/3gpp') /*'video/x-flv'*/
+		
+		{?>
+		<video src="<?php echo $fichier ; ?>" controls poster="./images/Video-Icon-crop-1.png" preload="none" alt ="<?php echo $alt ; ?> www.soullatitude.com" >  <?php echo $alt ; ?> www.soullatitude.com </video>
+		<p>vidéo de type: <?php echo $typeMime ; ?></p>
+
+			<?php 
+			if ($credit){ ?>
+			<p>Vidéo &copy <?php echo $credit ;?></p>
+			<?php }?>
+
+		<?php 
+		}
+
+	}
+		
+?>
