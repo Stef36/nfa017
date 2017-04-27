8fb5a0c287d7592d973b365242868224d6305478
domduf <minique.duf@gmail.com>
2017-04-27 22:09:32 +0200

    requete JOIN sur includes/attribution_conges.inc.php

--- includes/attribution_conges.inc.php
+++ includes/attribution_conges.inc.php
@@ -4,49 +4,56 @@
 <h2>ATTRIBUTION DES CONGES</h2>
     <form class="attribution_form"  Method="post" name="formulaire"  >
 
+
+        <ul>
     <?php /* requete selection des types congés */
 
             $sql_types_conges = "SELECT type_conge_id, type_conge_nom, type_conge_commentaire, type_conge_unite, type_conge_valable, type_conge_logo
-                                FROM type_conge
+                                FROM type_conge 
+                                WHERE type_conge_valable=1
                                  ;";
 
             $types_conges = $pdo-> query($sql_types_conges);
 
-            while ($type_conge=$types_conges->fetch()) {
+
+
+            $sql_employe_dispose_combiens_type_conges =
+                                "SELECT disposer_quantite,  disposer.type_conge_id , type_conge_nom, type_conge_commentaire, type_conge_unite, type_conge_valable,type_conge_logo, employe_id
+                                FROM disposer  JOIN type_conge 
+                                ON disposer.type_conge_id = type_conge.type_conge_id
+                                WHERE disposer.employe_id = $id_selection_employe;";
+
+
+            $employe_dispose_type_conges = $pdo-> query($sql_employe_dispose_combiens_type_conges);
 
 
 
-                echo "congé: ".$type_conge['type_conge_nom']."<br/>";
-                echo "congé: ".$type_conge['type_conge_unite']."<br/>";
+            while ($employe_dispose_type_conge=$employe_dispose_type_conges->fetch()) { 
+
+            //echo $employe_dispose_type_conge['type_conge_nom'];?>
+
+            <li><label for="number"><?php echo $employe_dispose_type_conge['type_conge_nom'];?></label>
+
+                <input type="number" id="<?php echo $employe_dispose_type_conge['type_conge_nom'];?>" name="<?php echo $employe_dispose_type_conge['type_conge_nom'];?>" size="3"  onblur="" 
+                value ="<?php  echo $employe_dispose_type_conge['disposer_quantite'];
+
+                
+
+
+
+                ?>">
+                <?php echo " ".$employe_dispose_type_conge['type_conge_unite']; ?> </li>
+            <BR> <?php
 
 
             }
 
      ?>
 
-
-        <ul>
-            <li><label for="number">Congés payés :</label>
-                <input type="text" id="conges_payes" name="conges_payes" size="3"  onblur=""> jours</li>
-            <BR>
-            <li><label for="number">Ancienneté :</label>
-                <input size="3" type="text" name="anciennete" id="anciennete"> jours</li>
-            <BR>
-            <li><label for="number">RTT :</label>
-                <input size="3" type="text" name="rtt" id="rtt"> jours</li>
-            <br>
-            <li><label for="number">Maladie :</label>
-                <input size="3" type="text" name="maladie" id="maladie"> jours</li>
-            <br>
-            <li><label for="number">Abscence non autorisée :</label>
-                <input size="3" type="text" name="abscence" id="abscence">
-            jours</li>
-            <br>
-            <li><label for="number">Formation :</label>
-                <input size="3" type="text" name="formation" id="formation"> jours
-            </li>
-            <br>
         </ul>
+
+
+
         <fieldset>
             <center>
                 <legend>Commentaires :</legend>
