51b145726162d3616e71f61660566a9b2e627f76
Stef <slaruelle@free.fr>
2017-04-30 21:36:35 +0200

    modification CSS

--- includes/selection_employe.inc.php
+++ includes/selection_employe.inc.php
@@ -5,7 +5,7 @@
 
     if (isset($_SESSION['ticket_equipe'])) 
     {
-    	
+        
   
                 // entrée en BD des modifications faites en "inscription_personnel.inc.php"
                 include("includes/inscription_personnel_VALID.inc.php");
@@ -34,32 +34,32 @@
      <form method="post" name="selection_employe">
 
 
-				<?php
-     			// si un choix a déjà été effectué, recupere l'employe_id
-     			// de ce choix:
-				 if (isset($_POST['select_employe'])) {
+                <?php
+                 // si un choix a déjà été effectué, recupere l'employe_id
+                 // de ce choix:
+                 if (isset($_POST['select_employe'])) {
 
-				 	// met la selection dans une variable
-     				$selection_post=$_POST['select_employe'];
-     				//echo $selection_post;
+                     // met la selection dans une variable
+                     $selection_post=$_POST['select_employe'];
+                     //echo $selection_post;
 
-     				// on cherche où commence "->"
-     				$findme   = '-';
-     				$pos = strpos($selection_post, $findme);
-     				//echo $pos;
+                     // on cherche où commence "->"
+                     $findme   = '-';
+                     $pos = strpos($selection_post, $findme);
+                     //echo $pos;
 
-     				// on coupe la chaine jusqu'au début de "  ->"
-     				// pour extraire id_employe
-     				$id_selection_employe= substr($selection_post, 0, ($pos-1));
+                     // on coupe la chaine jusqu'au début de "  ->"
+                     // pour extraire id_employe
+                     $id_selection_employe= substr($selection_post, 0, ($pos-1));
 
-     				if ($id_selection_employe=='nouvel employ') {
-				 		$id_selection_employe=NULL;
-				 	}
+                     if ($id_selection_employe=='nouvel employ') {
+                         $id_selection_employe=NULL;
+                     }
 
-     				//echo 'employe_id -'.$id_selection_employe.'-';
+                     //echo 'employe_id -'.$id_selection_employe.'-';
                     $_SESSION['id_selection_employe']=$id_selection_employe;
 
-     			};
+                 };
 
 
 
@@ -68,43 +68,43 @@
 
 
 
-     		<select name="select_employe" size="1">
+             <select name="select_employe" size="1">
 
-     			<option    <?php if (!isset($_POST['select_employe'])) 
-     								{
-     								$id_selection_employe=NULL;
-     								echo "selected"; 
-     								}; ?>  >nouvel employe
-     			</option>
-     			
-     			<?php 
-     				// boucle d'affichage pour ajouter chaque employé
-     				// à la liste déroulante
-     				while ( $employe_de_l_equipe=$employes_de_l_equipe->fetch()) {?>
+                 <option    <?php if (!isset($_POST['select_employe'])) 
+                                     {
+                                     $id_selection_employe=NULL;
+                                     echo "selected"; 
+                                     }; ?>  >nouvel employe
+                 </option>
+                 
+                 <?php 
+                     // boucle d'affichage pour ajouter chaque employé
+                     // à la liste déroulante
+                     while ( $employe_de_l_equipe=$employes_de_l_equipe->fetch()) {?>
 
-		     			<option  <?php if ( 
-		     				(isset($_POST['select_employe']))
-		     				 &
-		     				($id_selection_employe==$employe_de_l_equipe['employe_id'])) 
-		     				{
-		     				echo "selected"; // choix par défaut
-		     				}; ?> >
-		     			<?php echo $employe_de_l_equipe['employe_id'].' -> '. $employe_de_l_equipe['employe_nom'].' '.$employe_de_l_equipe['employe_prenom']; 
+                         <option  <?php if ( 
+                             (isset($_POST['select_employe']))
+                              &
+                             ($id_selection_employe==$employe_de_l_equipe['employe_id'])) 
+                             {
+                             echo "selected"; // choix par défaut
+                             }; ?> >
+                         <?php echo $employe_de_l_equipe['employe_id'].' -> '. $employe_de_l_equipe['employe_nom'].' '.$employe_de_l_equipe['employe_prenom']; 
 
-		     			?>
-		     				
-		     			</option> 
+                         ?>
+                             
+                         </option> 
 
 
-		     		<?php 
-     				} 
-     			?>
+                     <?php 
+                     } 
+                 ?>
 
-     			
-     		</select>
+                 
+             </select>
 
-     		<input type="submit" name="submit" value="OK">
-     	
+             <input type="submit" name="submit" value="OK">
+         
 
      </form> 
 
