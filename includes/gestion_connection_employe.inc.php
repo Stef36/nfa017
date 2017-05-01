db25ef57c2f1394e6aa5b2cd50fed248df5774db
domduf <minique.duf@gmail.com>
2017-05-01 11:17:41 +0200

    modifie header car inadapte au titre, creer includes comment_utiliser_employe.inc.php gestion_connection_employe.inc.php

---
+++ includes/gestion_connection_employe.inc.php
@@ -0,0 +1,67 @@
+<?php  Session_start();
+
+		// ================ Connection bdd via PDO ================
+ 		include("./includes/connection.php"); 
+		require('./includes/fonctions_utiles.php');//pour affichage des logos
+		// gestion de la connection
+
+
+	// si on tente de se connecter via le log équipe------------------
+
+	if (isset($_POST['login_equipe'])) {
+
+		$log=$_POST['login_equipe'];
+		$mdp=$_POST['equipe-passwd'];
+
+		// test login 
+		$sql_log_equipe = "SELECT equipe_id, equipe_login, equipe_mdp, 									equipe_nom, equipe_entreprise, equipe_responsable, equipe_mail, equipe_logo, equipe_visible
+							FROM 		equipe ;" ;
+		
+		$log_equipes= $pdo->query($sql_log_equipe);
+
+			//parcours la table des logs equipe
+			while ( $log_equipe=$log_equipes->fetch()) {
+
+				if (
+					($log_equipe['equipe_login']==$log)
+					&
+					($log_equipe['equipe_mdp']==$mdp)
+					& 
+					($log_equipe['equipe_mdp']!='')
+					&
+					($log_equipe['equipe_visible'])
+					) {
+
+					$_SESSION['ticket_equipe']=1;
+					$_SESSION['equipe_id']=$log_equipe['equipe_id'];
+					$_SESSION['equipe_login']=$log_equipe['equipe_login'];
+					$_SESSION['equipe_nom']=$log_equipe['equipe_nom'];
+					$_SESSION['equipe_entreprise']=$log_equipe['equipe_entreprise'];
+					$_SESSION['equipe_responsable']=$log_equipe['equipe_responsable'];
+					$_SESSION['equipe_mail']=$log_equipe['equipe_mail'];
+					$_SESSION['equipe_logo']=$log_equipe['equipe_logo'];
+
+
+
+					//echo '<p>OK équipe: '.$log_equipe['equipe_login'];'</p>';
+					}
+				
+			}
+
+		}
+
+
+	// si on tente de se déconnecter via le log équipe------------------
+
+		if (isset($_POST['deconnecter_equipe'])) {
+			if ($_POST['deconnecter_equipe']=='OK') {
+
+				// fermeture de session
+ 
+  				$_SESSION = array();
+  				session_destroy();
+			}
+		};
+
+
+ ?>
---
+++ mes_conges.php
@@ -0,0 +1,116 @@
+<!-- ================ Connection bdd via PDO ================ -->
+<?php include("./includes/gestion_connection_employe.inc.php");
+//affiche_variables_session() ?>
+
+
+<!-- ================ Connection bdd via PDO ================ -->
+<?php include("./includes/connection.php"); ?>
+
+<!DOCTYPE html>
+<html lang="fr-fr" >
+
+<!-- ======================================================= -->
+    <head>
+
+        <meta charset="UTF-8" lang="fr">
+        <meta name="Mes Repos"  content="poser consulter jours congés travail équipe ">
+
+        <link rel="icon" type="image/x-icon" href="./images/photos/favicon.ico">
+  
+        <title>Equipes de travail</title>
+        <link href="./css/style.css" rel="stylesheet" type="text/css" />
+        <link href="./css/stylea.css" rel="stylesheet" type="text/css" />
+    </head>
+<!-- ======================================================= -->
+
+
+    <body>
+
+
+
+
+
+<!-- ===================== TITRE ===================== 
+        <header class="headera">
+            <h1>LES EQUIPES DE TRAVAIL<H1>
+                    </header>-->
+<!-- ===================== TITRE ===================== -->
+
+
+<?php require ("./includes/header.inc.php"); titre_header('MES CONGES')?>
+
+
+
+
+<!--=======voir si possibilité===Menu de demande de validation================-->
+        <article id="warm">
+warm
+        
+        </article>
+<!-- ===================== MENU ===================== -->
+        <nav>
+        <?php include("includes/menu.php"); ?>
+    </nav>
+
+
+<?php //affiche_variables_session(); ?>
+<!--====================================================================-->
+<!--=========================COMMENT CREER UNE EQUIPE===================-->
+<!--====================================================================-->
+
+        <section id="creer_equipe" class="container">
+
+            <?php include("includes/comment_utiliser_employe.inc.php"); ?>
+        
+            <section class="flex">
+                <article class="flex1">
+equipe-presentation
+                <?php include("includes/equipe_presentation.inc.php"); ?>
+                </article>
+    
+<!--====================login=============================-->
+
+                <aside class="log_equipea">
+log_equipe
+                <?php include("includes/log_equipe.inc.php"); ?>
+
+                </aside>
+        
+            </section></section>
+<!--====================================================================-->
+<!--===========================DESCRIPTION EQUIPE=======================-->
+<!--====================================================================-->
+        <section id="description_equipe">
+        
+description_equipe
+        <?php include("includes/description_equipe.inc.php"); ?>
+        
+        
+<!--===========================EQUIPES=======================-->
+        <article id="equipes">
+equipes
+        <?php include("includes/equipes.inc.php"); ?>
+        </article>
+        
+        </section>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+<!-- ===================== BAS DE PAGE  ===================== -->
+        <?php include("includes/basDePage.php"); ?>
+    </body>
+
+</html>
