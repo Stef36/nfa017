c6be4f4ec382954707b3653dddd8c57a5884fd9d
domduf <minique.duf@gmail.com>
2017-05-08 20:44:13 +0200

    modif mise en page includes/conges_poser_employe.inc.php

--- includes/conges_poser_employe.inc.php
+++ includes/conges_poser_employe.inc.php
@@ -4,9 +4,123 @@
 // ---------   si aucun  employé n' est connectée-------------------------------
 if (! isset($_SESSION['ticket_employe'])) { ?>
 
-<h2>ICI bientôt un formulaire pour poser vos congés</h2>
 
 
+    <article>
+
+    <h2>Formulaire pour poser vos congés</h2>
+
+    <H2>
+        Une fois connecté, vous pourrez ici selectionner et poser vos congés:
+    </H2>
+
+        <section class="formulaire_pose_conge">
+
+        <form method="post" name="formulaire_pose_conge" >
+             
+
+            <div id="type_conge">                 <p>
+                    <input type="radio" 
+                            name="type_conge_id" 
+                            value="1" >
+
+                    CONGE PAYE en jour                <p>
+                    <input type="radio" 
+                            name="type_conge_id" 
+                            value="2" >
+
+                    ANCIENNETÉ en jour                <p>
+                    <input type="radio" 
+                            name="type_conge_id" 
+                            value="4" >
+
+
+                    SABBATIQUE en jour                <p>
+                    <input type="radio" 
+                            name="type_conge_id" 
+                            value="8" >
+
+                    RAISON FAMILIALE en jour                <p>
+                    <input type="radio" 
+                            name="type_conge_id" 
+                            value="9" >
+
+                    PARENTAL EDUCATION en jour                <p>
+                    <input type="radio" 
+                            name="type_conge_id" 
+                            value="10" >
+
+                    JRTT en jour                <p>
+                    <input type="radio" 
+                            name="type_conge_id" 
+                            value="11" >
+
+                    MALADIE en jour                <p>
+                    <input type="radio" 
+                            name="type_conge_id" 
+                            value="12" >
+
+                    ABSENCE NON AUTORISEE en heure 
+
+                    </input>
+                </p>
+                
+            </div>
+
+            <div id="date_conge">
+
+                <p> Date de début
+                <input type="date" 
+                name="conge_date" 
+                value="2017-05-08" >
+                </input>
+                
+                Heure éventuelle
+                <input type="time" 
+                name="conge_time" 
+                value="00:00:00" >
+
+                </input>
+                </p>
+
+
+            </div>
+
+            <div id="quantite_conge">
+
+            <p>De combien de congé avez-vous besoin ?</p>
+            <input 
+            type="number" 
+            name="conge_quantite"
+            min="0"
+            step="0.5"
+            >
+                
+            </div>
+
+            <div id="valide">
+            <p>Vous pouvez choisir de ne pas poser tout de suite</p>
+            <input type="radio" 
+            name="conge_demande" 
+            value="1" checked=""> Je pose
+            <input type="radio" 
+            name="conge_demande" 
+            value="0" > Je ne pose pas encore.          
+            </input>
+
+            <p>Vous pouvez entrer un commentaire:</p>
+            <input type="text" name="conge_commentaire"/>
+
+            </div>
+            
+
+            </form>
+        </section>
+        
+
+
+
+    </article>
 
 <p>Bons congés !</p>
 
@@ -17,27 +131,28 @@ if (! isset($_SESSION['ticket_employe'])) { ?>
 // ---------   si un employé est connectée-------------------------------
 
 else { ?>
- 	<h2>Formulaire de demande de congés.</h2>
+ 	
 
 
     <!--  =============== insertion en BD du congé================-->
     <!--  ===============     le cas échéant      ================-->
-    <p>
+    
         <?php if(isset($_POST['type_conge_id'])) {
 
 
-            echo 'type_conge_id->'.$_POST['type_conge_id'].' <br>';
+            //echo 'type_conge_id->'.$_POST['type_conge_id'].' <br>';
             $type_conge_id=$_POST['type_conge_id'];
 
-            echo 'conge_date->'.$_POST['conge_date'].' '.$_POST['conge_time'].' <br>';
+            //echo 'conge_date->'.$_POST['conge_date'].' '.$_POST['conge_time'].' <br>';
             $conge_datedebut=$_POST['conge_date'].' '.$_POST['conge_time'];
             
-            echo 'conge_quantite-> '.$_POST['conge_quantite'].' <br>';
+            //echo 'conge_quantite-> '.$_POST['conge_quantite'].' <br>';
             $conge_quantite=$_POST['conge_quantite'];
 
-            echo 'conge_commentaire-> '.$_POST['conge_commentaire'].' <br>';
+            //echo 'conge_commentaire-> '.$_POST['conge_commentaire'].' <br>';
+            $conge_commentaire=$_POST['conge_commentaire'];
 
-            echo 'conge_demande-> '.$_POST['conge_demande'].' <br>';
+            //echo 'conge_demande-> '.$_POST['conge_demande'].' <br>';
             $conge_demande=$_POST['conge_demande'];
 
             $employe_id=$_SESSION['employe_id'];
@@ -48,27 +163,29 @@ else { ?>
 
             // requete d'insertion en BD
             $sql_insert_conge= "INSERT INTO conge
-                                SET conge_datedebut=?, conge_quantite=?, conge_demande=?, employe_id=?, type_conge_id=? ; ";
+                                SET conge_datedebut=?, conge_quantite=?, conge_commentaire=?, conge_demande=?, employe_id=?, type_conge_id=? ; ";
 
-            $nouvelles_valeurs= array($conge_datedebut, $conge_quantite,$conge_demande, $employe_id, $type_conge_id );
+            $nouvelles_valeurs= array($conge_datedebut, $conge_quantite,$conge_commentaire, $conge_demande, $employe_id, $type_conge_id );
 
             // insert de l'enregistrement
             applique_requete($sql_insert_conge, $pdo, $nouvelles_valeurs);
 
             } ?>
-    </p>
+    
 
  	<!--  =================== article  ==========================-->
 
     <article>
 
-    <p>
+    <h2>Formulaire de demande de congés.</h2>
+
+    <H3>
         Vous pouvez ici selectionner et poser vos congés.
-    </p>
+    </H3>
 
-        <section class="formulaire_pose_conge">
+    <section class="formulaire_pose_conge">
 
-        <form method="post" name="formulaire_pose_conge" >
+        <form method="post" name="formulaire_pose_conge" class="flex_employe" >
              
 
             <div id="type_conge"> <?php
@@ -104,6 +221,7 @@ else { ?>
                 
             </div>
 
+            <div>
             <div id="date_conge">
 
                 <p> Date de début
@@ -111,8 +229,9 @@ else { ?>
                 name="conge_date" 
                 value="<?php echo date('Y-m-d')?>" >
                 </input>
+                </p>
                 
-                Heure éventuelle
+                <p>Heure éventuelle
                 <input type="time" 
                 name="conge_time" 
                 value="<?php echo "00:00:00"?>" >
@@ -134,6 +253,7 @@ else { ?>
             >
                 
             </div>
+            </div>
 
             <div id="valide">
             <p>Vous pouvez choisir de ne pas poser tout de suite</p>
@@ -142,7 +262,7 @@ else { ?>
             value="1" checked=""> Je pose
             <input type="radio" 
             name="conge_demande" 
-            value="0" > Je ne pose pas encore.          
+            value="NULL" > Je ne pose pas encore.          
             </input>
 
             <p>Vous pouvez entrer un commentaire:</p>
