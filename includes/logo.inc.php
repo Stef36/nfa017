2577723f921e8eb3582f22a67d93a97df9e88541
domduf <minique.duf@gmail.com>
2017-05-25 16:56:46 +0200

    FB et Linkedin sur includes/logo.inc.php et includes/basDePage.php

--- includes/logo.inc.php
+++ includes/logo.inc.php
@@ -1,19 +1,16 @@
 <?php include_once("./includes/analyticstracking.php") ?>
 
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.9";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
 
 <header >
  
     <a href="index.php"><img src="./images/Logo/MRepos.com40.png" alt="logo site mesrepos.domduf.com" class="logo"></a><br/>
 
-    <div class="fb-like" data-href="http://mesrepos.domduf.com/" data-width="20" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+    <div class="fb-like" data-href="http://mesrepos.domduf.com/" data-width="20" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div><br/>
+
+    
+	<script type="IN/Share" data-url="http://mesrepos.domduf.com/" data-counter="right"></script>
 
 </header>
 
