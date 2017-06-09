599c63cd7bd6a73c7bf4bd77f2740240832f29b2
domduf <minique.duf@gmail.com>
2017-06-09 16:58:54 +0200

    modif carte

--- javascripts/carteAPI.js
+++ javascripts/carteAPI.js
@@ -6,13 +6,13 @@ var siegeSocial;
 function initMap() {
 
 	// Une position par défaut (Châtelet à Orléans) voir http://www.coordonnees-gps.fr/communes
-//var chateletOrleans = new google.maps.LatLng({lat: 47.89927580000001, lng: 1.9065545999999358});
+  var neuville = new google.maps.LatLng({lat: 48.0686283, lng: 2.0533742000000075});
 
 	//création d'un objet siegeSocial
   siegeSocial = new google.maps.Map(document.getElementById('siegeSocial'), {
-    center: {lat: 48.1466075, lng: 1.6197907},
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    zoom: 4
+    center: neuville,
+    mapTypeId: google.maps.MapTypeId.HYBRID,
+    zoom: 16
   });
 
 
